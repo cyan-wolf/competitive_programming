68fd41194fee749de171ba4755acf78ead13fca1
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-10-11 15:04:02 -0400

    started working on problem F from the ICPC 2024 Caribbean Finals

---
+++ matcomgrader/icpc_caribbean_finals_2024/f.cpp
@@ -0,0 +1,24 @@
+/*
+    Problem F - Finding Privacy
+    https://scorelatam.naquadah.com.br/latam-2024/contest.pdf
+    https://scorelatam.naquadah.com.br/latam-2024/Score.html
+    https://codeforces.com/gym/105505/problem/F
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// void det_seq(int rem_space, int needed_people, char action, vector<int> acc_seq) {
+//     if (rem_space == 0) {
+//         return;
+//     }
+// }
+
+int main() {
+    int people_amt, toilet_amt;
+    cin >> people_amt >> toilet_amt;
+
+    // TODO: ...
+}
