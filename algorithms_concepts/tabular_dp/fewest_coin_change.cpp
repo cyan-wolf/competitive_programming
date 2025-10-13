fe8c7c49e97730f5db4a31d0b04ef3f5b4964bcf
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-10-13 19:47:02 -0400

    added tabular DP version of the coin change problem in C++

--- algorithms_concepts/tabular_dp/fewest_coin_change.cpp
+++ algorithms_concepts/tabular_dp/fewest_coin_change.cpp
@@ -1,9 +1,19 @@
+/**
+ * Given an array of coin denominations and a target amount, 
+ * find the minimum number of coins needed to make up that amount. 
+ * You can use each coin as many times as you want.
+ */
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
 
 using namespace std;
+
+// NOTE: `INT_MAX` represents an empty value (since any min operation on it on a regular number will make the `INT_MAX` disappear).
+// NOTE: -1 represents an error sentinel value, it is returned when a result cannot be computed using the given denominations 
+//        and target amout (i.e. trying to find the coin amount for {3, 5} when the target amount is 2).
 
 int count_fewest_coin_change_memo(const vector<int> &denominations, int target_amt, unordered_map<int, int> &cache) {
     if (cache.count(target_amt)) {
@@ -21,7 +31,7 @@ int count_fewest_coin_change_memo(const vector<int> &denominations, int target_a
             }
             int new_target_amt = target_amt - d;
             int min_amt_for_d = count_fewest_coin_change_memo(denominations, new_target_amt, cache);
-            
+
             // Skip failed subproblems.
             if (min_amt_for_d == -1) {
                 continue;
@@ -39,9 +49,35 @@ int count_fewest_coin_change_memo(const vector<int> &denominations, int target_a
     }
 }
 
+int count_fewest_coin_change_tabular(const vector<int> &denominations, int target_amt) {
+    vector<int> dp(target_amt + 1, INT_MAX);
+    dp[0] = 0;
+
+    for (int t = 1; t < target_amt + 1; ++t) {
+        for (int d : denominations) {
+            if (t < d) {
+                continue;
+            }
+            int new_target = t - d;
+            int min_for_d = dp[new_target];
+
+            if (min_for_d == -1) {
+                continue;
+            }
+            dp[t] = min(dp[t], 1 + min_for_d);
+        }
+        if (dp[t] == INT_MAX) {
+            dp[t] = -1;
+        }
+    }
+    return dp[target_amt];
+}
+
 int main() {
-    unordered_map<int, int> cache;
     vector<int> denominations = {3, 5, 7};
-    int amt = count_fewest_coin_change_memo(denominations, 8, cache);
+    // unordered_map<int, int> cache;
+    // int amt = count_fewest_coin_change_memo(denominations, 8, cache);
+
+    int amt = count_fewest_coin_change_tabular(denominations, 8);
     cout << amt << endl;
 }
