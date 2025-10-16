f9821cac19b0a58ea2efaa5c31925d9d8f316be8
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-10-15 20:14:41 -0400

    Started working on LeetCode Reverse Integer problem in C++

---
+++ leetcode/reverse_integer.cpp
@@ -0,0 +1,32 @@
+/**
+ * Reverse Integer
+ * https://leetcode.com/problems/reverse-integer/description/
+ * 
+ * Given a signed 32-bit integer x, return x with its digits reversed. 
+ * If reversing x causes the value to go outside the signed 32-bit integer 
+ * range [-231, 231 - 1], then return 0. Assume the environment does not allow 
+ * you to store 64-bit integers (signed or unsigned).
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int x) {
+    // TODO: ...
+    return -1;
+}
+
+// Unnecessary class for LeetCode submission.
+class Solution {
+public:
+    int reverse(int x) {
+        return solution(x);
+    }
+};
+
+int main() {
+    cout << solution(123) << endl;
+    cout << solution(-123) << endl;
+    cout << solution(121) << endl;
+}
