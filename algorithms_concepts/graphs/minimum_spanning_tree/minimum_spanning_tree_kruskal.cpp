4fed0e90faf98b85b2072169dfb84b65ff273a61
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-10-19 20:20:02 -0400

    started working on MST using Kruskal's algorithm

---
+++ algorithms_concepts/graphs/minimum_spanning_tree/minimum_spanning_tree_kruskal.cpp
@@ -0,0 +1,15 @@
+/**
+ * Minimum Spanning Tree - Kruskal
+ * 
+ * Finds the most economical way to connect a set of points (vertices) in a graph, 
+ * ensuring that all points are connected with the lowest possible total connection 
+ * cost (edge weights) and without forming any cycles.
+ */
+
+#include <iostream>
+
+// TODO: everything
+
+int main() {
+
+}
