f9a449826136c15bef053835115408f341ce8459
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-11-02 18:44:57 -0400

    added solution to 4Sum problem (got runtime error)

---
+++ leetcode/4sum.cpp
@@ -0,0 +1,85 @@
+/**
+ * 4Sum
+ * https://leetcode.com/problems/4sum/
+ * 
+ * Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ *      0 <= a, b, c, d < n
+ *      a, b, c, and d are distinct.
+ *      nums[a] + nums[b] + nums[c] + nums[d] == target
+ *      You may return the answer in any order.
+ * 
+ */
+
+// Runtime Error: Signed Integer Overflow on test case #283
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<int>& nums, int target) {
+    unordered_map<int, int> seen;
+    for (int i = 0; i < nums.size(); ++i) {
+        seen[nums[i]] = i;
+    }
+    set<vector<int>> solutions;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        for (int j = i + 1; j < nums.size(); ++j) {
+            for (int k = j + 1; k < nums.size(); ++k) {
+                if (i == j || j == k || i == k) {
+                    continue;
+                }
+                int needed = target - nums[i] - nums[j] - nums[k];
+
+                auto it = seen.find(needed);
+                if (it != seen.end() && it->second > k) {
+                    vector<int> quadruplet = {nums[i], nums[j], nums[k], it->first};
+                    sort(quadruplet.begin(), quadruplet.end());
+
+                    solutions.insert(quadruplet);
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> solutions_vec;
+    for (auto q : solutions) {
+        solutions_vec.push_back(q);
+    }
+    return solutions_vec;
+}
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        return solution(nums, target);
+    }
+};
+
+int main() {
+    vector<int> nums = {1,0,-1,0,-2,2};
+    auto solutions = solution(nums, 0);
+
+    for (auto quadruplets : solutions) {
+        for (int n : quadruplets) {
+            cout << n << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    nums = {2,2,2,2,2};
+    solutions = solution(nums, 8);
+
+    for (auto quadruplets : solutions) {
+        for (int n : quadruplets) {
+            cout << n << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
